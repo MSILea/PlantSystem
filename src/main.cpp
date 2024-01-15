1947d9ae869bc80e77be73d43b2d3bf750a1615f
MSILea <you@example.com>
2024-01-15 19:14:20 +0100

    Website Lea

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+//Set pin number
+int moistSensPin = 36;
+int moistValue = 0;
+
+void setup() {
+  Serial.begin(9600); //start a serial connection with a baud rate of 9600
+}
+
+void loop() {
+//Save the moisture value to a variable
+moistValue = analogRead(moistSensPin);
+
+//Print the value to the serial output
+Serial.println(moistValue);
+
+//Wait for one second
+delay(1000);
+}
